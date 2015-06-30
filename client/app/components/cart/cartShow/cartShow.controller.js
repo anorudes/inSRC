3bac5ceb29bafd3f9ee5920d8870485d2141d93d
Maxim Varovin <anorudes@gmail.com>
2015-06-30 22:01:49 +0500

    deleted

--- client/app/components/cart/cartShow/cartShow.controller.js
+++
@@ -1,9 +0,0 @@
-class CartShowController {
-	constructor($scope, CartService, $stateParams){
-		$scope.cart = CartService.getOne($stateParams.id);
-	}
-}
-
-CartShowController.$inject = ['$scope','CartService', '$stateParams'];
-
-export default CartShowController;
