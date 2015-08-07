1f22ce0c4b9001a2f1dd9af36b34920da2d10a86
Maxim Varovin <anorudes@gmail.com>
2015-08-08 00:14:58 +0500

    fix

--- client/app/common/notePreview/notePreview.controller.js
+++ client/app/common/notePreview/notePreview.controller.js
@@ -2,18 +2,18 @@ class NotePreviewController {
   constructor(NoteService, ConfigService, ngDialog, $scope) {
     this.wordWrap = ConfigService.configData.wordWrap;
 
-    $scope.delete = () => {
+    this.delete = () => {
       NoteService.delete(this.note.id);
       this.closePreview();
     };
 
     this.deleteModal = () => {
-      var nestedConfirmDialog = ngDialog.openConfirm({
+      let nestedConfirmDialog = ngDialog.openConfirm({
         template:'\
             <p>Would you like to delete?</p>\
             <div class="ngdialog-buttons">\
                 <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">No</button>\
-                <button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="delete(); closeThisDialog(0)">Yes</button>\
+                <button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="vm.delete(); closeThisDialog(0)">Yes</button>\
             </div>',
         scope: $scope,
         plain: true
